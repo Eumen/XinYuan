3b622bb968d512c3a0ec0db71c9655cc99812804
Administrator <Administrator@WIN-01804101510>
2018-04-15 10:21:58 +0800

    主页众筹基金显示

--- Vip/Lib/Action/PublicAction.class.php
+++ Vip/Lib/Action/PublicAction.class.php
@@ -254,7 +254,7 @@ class PublicAction extends CommonAction
         }
         import('@.ORG.RBAC');
         $fck = M('member');
-        $field = 'id,user_id,user_name,password,register_time,last_login_time,is_agent,cash,point';
+        $field = 'id,user_id,user_name,password,password2,register_time,last_login_time,is_agent,cash,point,bk8';
         $authInfo = $fck->where($map)->field($field)->find();
         // 使用用户名、密码和状态的方式进行认证
         if (false == $authInfo) {
@@ -272,6 +272,7 @@ class PublicAction extends CommonAction
             $_SESSION['login_isAgent'] = $authInfo['is_agent']; // 是否服务中心
             $_SESSION['cash'] = $authInfo['cash']; // 现金币
             $_SESSION['point'] = $authInfo['point']; // 积分币
+            $_SESSION['bk8'] = $authInfo['bk8']; // 基金
             $_SESSION['UserMktimes'] = mktime();
             // 身份确认 = 用户名+识别字符+密码
             $_SESSION['login_sf_list_u'] = md5($authInfo['user_id'] . 'wodetp_new_1012!@#' . $authInfo['password'] . $_SERVER['HTTP_USER_AGENT']);
